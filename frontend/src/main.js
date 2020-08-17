01fe833219602bfc35a58390e327b0d1788d76c3
Fabricio <41438768+Fabricio2210@users.noreply.github.com>
2020-08-17 13:37:31 -0300

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,6 +10,7 @@ import { faTelegram, faWhatsapp, faGithub } from '@fortawesome/free-brands-svg-i
 import Icon from 'vue-awesome/components/Icon'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import axios from 'axios'
+import VueScrollTo from 'vue-scrollto'
 
 library.add(faTelegram)
 library.add(faWhatsapp)
@@ -20,9 +21,10 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
+Vue.use(VueScrollTo)
 Vue.use(require('vue-moment'));
 
-//axios.defaults.baseURL = 
+axios.defaults.baseURL = 
 new Vue({
   router,
   render: h => h(App)
