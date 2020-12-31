3c7cd4ce848d5433dbb668e37b091f6259d2d3c5
Fabricio2210 <fabricio215@hotmail.com>
2020-12-31 16:57:24 -0300

    refatorando scripts

--- scripts/scrapVideoInfo.js
+++ scripts/scrapVideoInfo.js
@@ -2,7 +2,6 @@ const conectDB = require('./db/connectDB');
 const subjectPrompt = require('./services/prompts/subjectPrompt');
 const chalk = require('chalk');
 const figlet = require('figlet');
-require('dotenv').config();
 
 conectDB();
 console.log(
--- scripts/services/youtubeScrapData.js
+++ scripts/services/youtubeScrapData.js
@@ -1,51 +1,63 @@
 const fs = require('fs');
 const getSubtitles = require('youtube-captions-scraper').getSubtitles;
-const UserData = require('./helpers/UserData/UserData')
+const UserData = require('./helpers/VideoData/VideoData')
 const SubtitleData = require('./helpers/SubtitleData/SubTilteData')
+
 let counter = 0
-const youtubeScrap = (subject)=>{
-  fs.readdir(`./${subject}`,(err,data)=>{
+
+const youtubeScrap = (subject) => {
+  readFolder(subject)
+}
+
+const readFolder = (subject) =>{
+  fs.readdir(`./${subject}`,(err,dataFolder)=>{
     if(err){
       console.log('Erro')
     };
-    setInterval(()=>{
-      if(counter < data.length){
-        fs.readFile(`./${subject}/${data[counter]}`,(err,data)=>{
-          if(err){
-            console.log('erro na leitura do arquivo')
-          }
-          let { id,channel_id,channel_url,title,uploader,upload_date,thumbnail } = JSON.parse(data)
-          const getSubs = async ()=>{
-            try{
-              const subtitles = await getSubtitles({
-                videoID: id,
-                lang: 'en'
-              })
-               subtitles.forEach((data)=>{
-                 let subtitleId = `${id}${data.start}${data.dur}`
-                  SubtitleData(data.start,data.dur,data.text,subtitleId,id,channel_id,channel_url,title,uploader,upload_date,thumbnail,subject)
-              })
-             
-              return UserData(id,channel_id,channel_url,title,uploader,upload_date,thumbnail,subject)
-            }
-            catch (error) {
-              if(error.message == `Could not find captions for video: ${id}`){
-                console.log('VAI SE FUDER!!!')
-                 UserData(id,channel_id,channel_url,title,uploader,upload_date,thumbnail,subject)
-              }
-              console.log(`Error getting captions: ${error.message}`);
-            }
-           
-          }
-          (async()=>{
-            const subtitlesfinal = await getSubs()
-          })()
-        })
-        
-        counter++;
-      }else
-        return;
-    }, 5000);     
-  });
-};
+   readFiles(dataFolder,subject)
+  })
+}
+
+const readFiles = (dataFolder,subject) =>{
+  setInterval(() =>{
+    if(counter < dataFolder.length){
+      fs.readFile(`./${subject}/${dataFolder[counter]}`,(err,dataFile) =>{
+        if(err){
+          console.log('Erro na leitura do arquivo')
+        }
+        parseDataFile(dataFile,subject)
+      })
+      counter++
+    }
+  },5000)
+}
+
+const parseDataFile = (dataFile,subject) =>{
+  let { id,channel_id,channel_url,title,uploader,upload_date,thumbnail } = JSON.parse(dataFile)
+  getSubs(id,channel_id,channel_url,title,uploader,upload_date,thumbnail,subject)
+}
+
+const getSubs = async (id,channel_id,channel_url,title,uploader,upload_date,thumbnail,subject) =>{
+  const subtitles = await getSubtitles({
+    videoID: id,
+    lang: 'en'
+  })
+  parseSubs(id,channel_id,channel_url,title,uploader,upload_date,thumbnail,subtitles,subject)
+}
+
+const parseSubs = (id,channel_id,channel_url,title,uploader,upload_date,thumbnail,subtitles,subject) =>{
+  try{
+    subtitles.forEach((dataSub) =>{
+      let subtitleId = `${id}${dataSub.start}${dataSub.dur}`
+      SubtitleData(dataSub.start,dataSub.dur,dataSub.text,subtitleId,id,channel_id,channel_url,title,uploader,upload_date,thumbnail,subject)
+    })
+    UserData(id,channel_id,channel_url,title,uploader,upload_date,thumbnail,subject)
+  }catch(error){
+    if(error.message == `Could not find captions for the video: ${id}`){
+      UserData(id,channel_id,channel_url,title,uploader,upload_date,thumbnail,subject)
+   }
+   console.log(`Error getting captions: ${error.message}`);
+  }
+  
+}
 module.exports = youtubeScrap
